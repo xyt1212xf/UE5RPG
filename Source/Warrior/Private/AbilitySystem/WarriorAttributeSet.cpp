a536bd39ec75d987f7bcec601b7c81c41d993ed2
18574567037 <2449437913@qq.com>
2025-05-07 13:29:55 +0800

    轻重攻击的伤害流程

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AbilitySystem/WarriorAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "WarriorDebugHelper.h"
 
 UWarriorAttributeSet::UWarriorAttributeSet()
 {
@@ -11,4 +13,37 @@ UWarriorAttributeSet::UWarriorAttributeSet()
 	InitMaxRage(1.0f);
 	InitAttackPower(1.0f);
 	InitDefensePower(1.0f);
+}
+
+void UWarriorAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute == GetCurrentHealthAttribute())
+	{
+		const float NewCurrentHealth = FMath::Clamp(GetCurrentHealth(), 0.f, GetMaxHealth());
+		SetCurrentHealth(NewCurrentHealth);
+	}
+	if (Data.EvaluatedData.Attribute == GetCurrentRageAttribute())
+	{
+		const float NewCurrentRage = FMath::Clamp(GetCurrentRage(), 0.f, GetMaxRage());
+		SetCurrentHealth(NewCurrentRage);
+	}
+
+	if (Data.EvaluatedData.Attribute == GetDamageTakenAttribute())
+	{
+		const float OldHealth = GetCurrentHealth();
+		const float DamageDone = GetDamageTaken();
+
+		const float NewCurrentHealth = FMath::Clamp(OldHealth - DamageDone, 0, GetMaxHealth());
+		SetCurrentHealth(NewCurrentHealth);
+		const FString DebugString = FString::Printf(
+			TEXT("Old Health = %f, Damage = %f, NewCurrentHealth = %f"),
+			OldHealth,
+			DamageDone,
+			NewCurrentHealth);
+		Debug::Print(DebugString, FColor::Green);
+		if (NewCurrentHealth == 0.f)
+		{
+
+		}
+	}
 }
--- Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
@@ -21,6 +21,8 @@ class WARRIOR_API UWarriorAttributeSet : public UAttributeSet
 	GENERATED_BODY()
 public:
 	UWarriorAttributeSet();
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
     UPROPERTY(BlueprintReadOnly,Category = "Health")
     FGameplayAttributeData  CurrentHealth;
     ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentHealth)
