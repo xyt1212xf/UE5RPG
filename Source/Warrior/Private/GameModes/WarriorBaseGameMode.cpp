02c7d463e2e2067c22f2329c3da9cef88acd468a
xiefantian1981 <xiefantian1981@gmail.com>
2025-12-11 13:21:18 +0800

    提交一下代码。修改一点点。

--- Source/Warrior/Private/GameModes/WarriorBaseGameMode.cpp
+++ Source/Warrior/Private/GameModes/WarriorBaseGameMode.cpp
@@ -5,6 +5,14 @@
 
 AWarriorBaseGameMode::AWarriorBaseGameMode()
 {
+	TArray<int >a ;
+	a.Push(222);
+	a.SetNum(10);
+	TArray<int >b;
+	b.Push(222);
+	b.SetNum(10);
+
+	auto xx = TNumericLimits<int32>::Max();
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
 }
